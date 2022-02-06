6a4e80213d6a3cd2e8e097eb991238edc0f6f634
juan-augusto <juanaugusto1@live.com>
2022-02-05 23:54:50 -0300

    Login page validation

---
+++ olx/frontend/src/Components/Helpers/OlxApi.jsx
@@ -0,0 +1,11 @@
+
+
+const olxAPI = {
+    login:async (email, password) =>{
+        //fazer consulta
+        return{
+            error:'Funcionalidade incompleta'
+        };
+    }
+}
+export default () => olxAPI;
--- olx/frontend/src/Components/Partials/LoginPage/LoginPage.jsx
+++ olx/frontend/src/Components/Partials/LoginPage/LoginPage.jsx
@@ -1,35 +1,81 @@
 
 import { LoginArea } from "./styled";
 import { PageContainer, PageTitle } from "../../MainComponents";
+import { useState } from "react";
+import useAPI from '../../Helpers/OlxApi'
+import { doLogin } from "../../Helpers/AuthHandler";
+import { ErrorMessage } from "../../MainComponents";
 
 export const LoginPage = () => {
+    const api = useAPI();
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword] = useState('');
+    const [ rememberPassword, setRememberPassword ] = useState('');
+    const [ disabled, setDisabled ] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        setDisabled(true);
+
+        const json = await api.login(email, password);
+        if(json.error) {
+            setError(json.error);
+        }else{
+            doLogin(json.token, rememberPassword); //saves cookie
+            window.location.href = '/'
+        }
+    }
+
     return(
         <PageContainer>
             <PageTitle>Login</PageTitle>
             <LoginArea>
-                <form>
+                {error &&
+                    <ErrorMessage>
+                        {error}
+                    </ErrorMessage>
+                }
+                <form onSubmit={handleSubmit}>
                     <label className="area">
                         <div className="area--title">E-mail</div>
                         <div className="area--input">
-                            <input type="email" />
+                            <input 
+                                type="email" 
+                                disabled={disabled} 
+                                value={email}
+                                onChange={e=>setEmail(e.target.value)}    
+                                required
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title">Senha</div>
                         <div className="area--input">
-                            <input type="password" />
+                            <input 
+                                type="password" 
+                                disabled={disabled}
+                                value={password}
+                                onChange={e=>setPassword(e.target.value)} 
+                                required  
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title">Lembrar senha</div>
                         <div className="area--input">
-                            <input type="checkbox" />
+                            <input 
+                                type="checkbox" 
+                                disabled={disabled}
+                                checked={rememberPassword}
+                                onChange={()=>setRememberPassword(!rememberPassword)}
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title"></div>
                         <div className="area--input">
-                            <button>Fazer Login</button>
+                            <button disabled={disabled}>Fazer Login</button>
                         </div>
                     </label>
                 </form>  
